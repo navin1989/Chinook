41a8e0fdb1e10db83ba9816f73158131ce00dca8
navin1989 <navinsx1989@gmail.com>
2023-08-21 10:30:43 +0530

    Add files via upload
    commit 2 - additional files

---
+++ Chinook/Hubs/SignalHub.cs
@@ -0,0 +1,13 @@
+﻿using Chinook.ClientModels;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chinook.Hubs
+{
+    public class SignalHub : Hub
+    {
+        public async Task SendSignal(PlaylistTrack track , string listName )
+        {
+            await Clients.All.SendAsync("ReceivedSignal", track, listName);
+        }
+    }
+}
