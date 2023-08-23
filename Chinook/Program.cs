3afcb2d509f459c0a531c090b6ed33aaf1e15b83
navin1989 <navinsx1989@gmail.com>
2023-08-23 10:09:55 +0530

    Add files via upload
    Bug fixes, code refactoring,
    Added auto mapper - ViewModel

--- Chinook/Program.cs
+++ Chinook/Program.cs
@@ -1,8 +1,13 @@
 using Chinook;
 using Chinook.Areas.Identity;
+using Chinook.ClientModels;
 using Chinook.Models;
+using Chinook.Services;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +18,18 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<ChinookUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ChinookContext>();
 
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<IArtistsRepository , ArtistsRepository>();
+builder.Services.AddScoped<IAlbumRepository , AlbumRepository>();
+builder.Services.AddScoped<IPlayListsRepository, PlayListsRepository>();
+builder.Services.AddScoped<ArtistData>();
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ChinookUser>>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -31,7 +44,6 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
