bef9a976052eca3ec199d7bc1108dbe2b990d92b
navin1989 <navinsx1989@gmail.com>
2023-08-24 13:36:46 +0530

    Delete Chinook/ClientModels directory

--- Chinook/ClientModels/Artist.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chinook.ClientModels
-{
-    public class Artist
-    {
-        public long ArtistId { get; set; }
-        public string? Name { get; set; }
-        public virtual ICollection<Album> Albums { get; set; }
-    }
-}
--- Chinook/ClientModels/ArtistVIewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Chinook.ClientModels
-{
-    public class ArtistViewModel
-    {
-        public string Name { get; set; } = string.Empty;
-    }
-}
